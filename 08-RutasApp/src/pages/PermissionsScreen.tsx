438496acdcff9a753650e525186ef46130cf070c
isaac aguilera <oiazisaac@gmail.com>
2021-10-05 18:21:31 -0400

    283 permiso de GPS al regresar a la aplicación

---
+++ 08-RutasApp/src/pages/PermissionsScreen.tsx
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react';
+import {Button, Platform, StyleSheet, Text, View} from 'react-native';
+import {PERMISSIONS, PermissionStatus, request} from 'react-native-permissions';
+import {PermissionsContext} from '../context/PermissionsContext';
+
+export const PermissionsScreen = () => {
+  const {permissions, askLocationPermission} = useContext(PermissionsContext);
+
+  return (
+    <View style={styles.container}>
+      <Text>PermissionsScreen</Text>
+      <Button title="Permiso" onPress={askLocationPermission} />
+      <Text>{JSON.stringify(permissions, null, 5)}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
